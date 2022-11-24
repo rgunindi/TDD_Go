1de3e5a521934a5bb112febdcd5563744efc2a18
Ramazan <codmyhesap@gmail.com>
2022-11-25 01:36:57 +0300

    modified:   hello.go 	modified:   hello_test.go

--- hello/hello.go
+++ hello/hello.go
@@ -1,9 +1,20 @@
 package main
 
-func Hello() string {
-	return "hello"
-}
+const prefix="hello"
+var language= make(map[string]string)
+func init(){
+	language["Spanish"]="Hola"
+	language["French"]="Bonjour"
 
-func main() {
-println(Hello())	
+}
+var found = func(key string) bool { _, ok := language[key]; return ok }
+func Hello(name string,lang string) string {
+	if name=="" {
+		name="world"
+	}
+	switch {
+	case  found(lang):
+		return language[lang]+", "+name
+	}
+	return prefix+" "+name
 }
--- hello/hello_test.go
+++ hello/hello_test.go
@@ -3,10 +3,31 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got:=Hello()
-	want:="hello"
-
+	assertCorrectMessage:= func (t testing.TB,got,want string) {
+		//Helper marks the calling function as a test helper function. When printing file and line information, that function will be skipped. Helper may be called simultaneously from multiple goroutines.
+		t.Helper()
 	if got!=want {
-		t.Errorf("got %q want %q", got, want)
+			t.Errorf("got %q want %q", got, want)
+		}
 	}
+	t.Run("say hello with name",func(t *testing.T) {
+		got:=Hello("John","")
+		want:="hello John"
+		assertCorrectMessage(t,got,want)	
+	})
+	t.Run("say hello, support empty value",func(t *testing.T) {
+		got:=Hello("","")
+		want:="hello world"
+		assertCorrectMessage(t,got,want)	
+	})
+	t.Run("in Spanish",func(t *testing.T) {
+		got:=Hello("Sara","Spanish")
+		want:="Hola, Sara"
+		assertCorrectMessage(t,got,want)
+	})
+	t.Run("in French",func(t *testing.T) {
+		got:=Hello("Sarah","French")
+		want:="Bonjour, Sarah"
+		assertCorrectMessage(t,got,want)
+	})
 }
