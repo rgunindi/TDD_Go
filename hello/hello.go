15cc7633dafe89b5bb12c947455f123626bc2572
Ramazan <codmyhesap@gmail.com>
2022-11-25 01:43:42 +0300

    modified:   hello.go

--- hello/hello.go
+++ hello/hello.go
@@ -1,20 +1,22 @@
 package main
 
-const prefix="hello"
-var language= make(map[string]string)
-func init(){
-	language["Spanish"]="Hola"
-	language["French"]="Bonjour"
+const prefix = "hello"
+var language = make(map[string]string)
 
+func init() {
+	language["Spanish"] = "Hola"
+	language["French"] = "Bonjour"
 }
+
 var found = func(key string) bool { _, ok := language[key]; return ok }
-func Hello(name string,lang string) string {
-	if name=="" {
-		name="world"
+func Hello(name string, lang string) string {
+	if name == "" {
+		name = "world"
 	}
 	switch {
-	case  found(lang):
-		return language[lang]+", "+name
+	case found(lang):
+		return language[lang] + ", " + name
+	default:
+		return prefix + " " + name
 	}
-	return prefix+" "+name
 }
