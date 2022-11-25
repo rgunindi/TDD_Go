345b6ba3ae94bdf7ddfcaf1ea16fdb7a8935e102
Ramazan <codmyhesap@gmail.com>
2022-11-25 09:06:17 +0300

    modified:   iteration.go 	modified:   iteration_test.go

--- iteration/iteration_test.go
+++ iteration/iteration_test.go
@@ -1,13 +1,27 @@
 package iteration
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
-func TestIteration(t *testing.T){
-got:=Iterate("a",5)
-expected:="aaaaa"
+func TestIteration(t *testing.T) {
+	got := Iterate("a", 5)
+	expected := "aaaaa"
 
-if got!=expected {
-	t.Errorf("Expected %q but got %q",expected,got)
+	if got != expected {
+		t.Errorf("Expected %q but got %q", expected, got)
+	}
 }
 
+func BenchmarkIteration(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Iterate("a", i)
+	}
+}
+
+func ExampleIteration() {
+	got := Iterate("b", 1)
+	fmt.Println(got)
+	// Output b
 }
