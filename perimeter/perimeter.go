7b3523ce1fddeccf639bdc389b798327ce56da57
Ramazan <codmyhesap@gmail.com>
2022-11-27 18:46:18 +0300

    table driven tests

--- perimeter/perimeter.go
+++ perimeter/perimeter.go
@@ -1,6 +1,7 @@
 package perimeter
 
 import "math"
+
 type Circle struct {
 	Radius float64
 }
@@ -8,9 +9,14 @@ type Rectangle struct {
 	width  float64
 	height float64
 }
-type Shape interface{
+type Triangle struct {
+	Base   float64
+	Height float64
+}
+type Shape interface {
 	Area() float64
 }
+
 func (r Rectangle) Area() float64 {
 	d := Rectangle{
 		width:  r.width,
@@ -18,8 +24,11 @@ func (r Rectangle) Area() float64 {
 	}
 	return d.width * d.height
 }
-func (c Circle) Area() float64{
-	return math.Pow(c.Radius,2.0)  * math.Pi
+func (c Circle) Area() float64 {
+	return math.Pow(c.Radius, 2.0) * math.Pi
+}
+func (t Triangle) Area() float64 {
+	return (t.Base * t.Height) * 0.5
 }
 func Perimeter(r Rectangle) float64 {
 	return 2 * (r.width + r.height)
--- perimeter/perimeter_test.go
+++ perimeter/perimeter_test.go
@@ -2,6 +2,8 @@ package perimeter
 
 import "testing"
 
+// run
+// go test -run TestArea/Rectangle
 func TestPerimeter(t *testing.T) {
 	got := Perimeter(struct {
 		width  float64
@@ -10,9 +12,9 @@ func TestPerimeter(t *testing.T) {
 		width:  10.0,
 		height: 10.0,
 	})
-	want := 40.0
-	if got != want {
-		t.Errorf("got %.2f want %.2f", got, want)
+	hasArea := 40.0
+	if got != hasArea {
+		t.Errorf("got %.2f hasArea %.2f", got, hasArea)
 	}
 }
 
@@ -26,25 +28,47 @@ func TestPerimeter(t *testing.T) {
 // },
 
 func TestArea(t *testing.T) {
-	checkArea := func(t testing.TB, shape Shape, want float64) {
-		t.Helper()
-		got := shape.Area()
-		if got != want {
-			t.Errorf("got %g want %g", got, want)
-		}
+	//Table driven tests
+	areaTests := []struct {
+		name    string
+		shape   Shape
+		hasArea float64
+	}{
+		{name: "Rectangle", shape: Rectangle{12, 6}, hasArea: 72.0},
+		{name: "Circle", shape: Circle{Radius: 10}, hasArea: 314.1592653589793},
+		{name: "Triangle", shape: Triangle{Base: 122, Height: 6}, hasArea: 36.0},
 	}
-	t.Run("rectangles", func(t *testing.T) {
-		rectangle := Rectangle{width: 12.0, height: 6.0}
-		//Function implementing
-		// got := Area(rectangle)
-		//method implementing
-		want := 72.0
-		checkArea(t, rectangle, want)
-	})
-	t.Run("circles", func(t *testing.T) {
-		circle := Circle{10}
-		want := 314.1592653589793
-		checkArea(t, circle, want)
-	})
-
+	for _, tt := range areaTests {
+		//we used tt.name for t.Run spec name
+		t.Run(tt.name, func(t *testing.T) {
+			got := tt.shape.Area()
+			if got != tt.hasArea {
+				t.Errorf("%#v got %g hasArea %g", tt.shape, got, tt.hasArea)
+			}
+		})
+	}
 }
+
+// {
+// 	checkArea := func(t testing.TB, shape Shape, hasArea float64) {
+// 		t.Helper()
+// 		got := shape.Area()
+// 		if got != hasArea {
+// 			t.Errorf("got %g hasArea %g", got, hasArea)
+// 		}
+// 	}
+// 	t.Run("rectangles", func(t *testing.T) {
+// 		rectangle := Rectangle{width: 12.0, height: 6.0}
+// 		//Function implementing
+// 		// got := Area(rectangle)
+// 		//method implementing
+// 		hasArea := 72.0
+// 		checkArea(t, rectangle, hasArea)
+// 	})
+// 	t.Run("circles", func(t *testing.T) {
+// 		circle := Circle{10}
+// 		hasArea := 314.1592653589793
+// 		checkArea(t, circle, hasArea)
+// 	})
+
+// }
